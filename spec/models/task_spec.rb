747feb60ac7b09c7a759b922e4460b0ede5ac453
Brian <riceb53@gmail.com>
2019-08-13 19:18:46 -0500

    add tests for Task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#toggle_complete!' do
+    it 'should change complete status from false to true' do
+      task = Task.create!(complete: false)
+      task.toggle_complete!
+      expect(task.complete).to eq(true)
+    end
+    it 'should change complete status from true to false' do
+      task = Task.create!(complete: true)
+      task.toggle_complete!
+      expect(task.complete).to eq(false)
+    end
+  end
 end
